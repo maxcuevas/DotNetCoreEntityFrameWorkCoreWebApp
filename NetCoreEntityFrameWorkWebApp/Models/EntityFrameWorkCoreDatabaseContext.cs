4af7eb2fde2f1c3f1ed145cb466f573e18f547f1
Max Cuevas <maximilianocuevas777@gmail.com>
2019-06-15 12:51:51 -0700

    Add code

---
+++ NetCoreEntityFrameWorkWebApp/Models/EntityFrameWorkCoreDatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NetCoreEntityFrameWorkWebApp.Models
+{
+    public partial class EntityFrameWorkCoreDatabaseContext : DbContext
+    {
+        public EntityFrameWorkCoreDatabaseContext()
+        {
+        }
+
+        public EntityFrameWorkCoreDatabaseContext(DbContextOptions<EntityFrameWorkCoreDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Table> Table { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EntityFrameWorkCoreDatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Table>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Age)
+                    .HasColumnName("age")
+                    .HasMaxLength(10);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ NetCoreEntityFrameWorkWebApp/Models/Table.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetCoreEntityFrameWorkWebApp.Models
+{
+    public partial class Table
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
